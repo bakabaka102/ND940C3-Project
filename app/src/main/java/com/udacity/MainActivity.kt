5d5dd5fcb890771e2eda858a870d55d0b17bfded
H Ngg <hnn@gmail.com>
2024-04-12 01:01:02 +0700

    Implement download, notify - Update shape of button - Implement channel notification

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -11,6 +11,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -20,6 +21,7 @@ import com.udacity.utils.Constants
 import com.udacity.utils.Logger
 import java.util.Date
 
+
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     private var downloadID: Long = 0
@@ -27,6 +29,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+    private var mToast: Toast? = null
 
     override fun instanceViewBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater)
@@ -52,6 +55,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         }
     }
 
+    private fun showToast(text: String?) {
+        mToast?.cancel()
+        mToast = Toast.makeText(this, text, Toast.LENGTH_SHORT)
+        //mToast?.setText(text)
+        mToast?.show()
+    }
+
     private fun showNotification() {
         val notification: Notification =
             NotificationCompat.Builder(this, Constants.CHANNEL_NOTIFY_ID)
@@ -69,6 +79,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             NotificationManagerCompat.from(this@MainActivity)
                 .notify(getNotificationId(), notification)
         } else {
+            showToast("POST_NOTIFICATIONS is denied")
             Logger.d("POST_NOTIFICATIONS is denied.")
         }
     }
@@ -91,6 +102,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mToast?.cancel()
     }
 
     override fun onDestroy() {
